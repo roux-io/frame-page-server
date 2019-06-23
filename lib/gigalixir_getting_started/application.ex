eebc3406ca9bdf80ae6dc70331cab386f68f2e84
Piers Mana <piers@piersmana.com>
2019-06-23 13:24:05 -0700

    Update application.ex

--- lib/gigalixir_getting_started/application.ex
+++ lib/gigalixir_getting_started/application.ex
@@ -8,8 +8,6 @@ defmodule GigalixirGettingStarted.Application do
 
     # Define workers and child supervisors to be supervised
     children = [
-      # Start the Ecto repository
-      supervisor(GigalixirGettingStarted.Repo, []),
       # Start the endpoint when the application starts
       supervisor(GigalixirGettingStartedWeb.Endpoint, []),
       # Start your own worker by calling: GigalixirGettingStarted.Worker.start_link(arg1, arg2, arg3)
